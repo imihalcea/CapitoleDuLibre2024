dc055e3e2baac5e9a910a06ceb4d6ac41223de1d
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-10-26 12:56:46 +0200

    Spike Read CPU Temperature

---
+++ spike_read_sensors/SpikeSensors/CpuTempReader.cs
@@ -0,0 +1,43 @@
+namespace SpikeSensors;
+
+using System;
+using System.IO;
+
+public class CpuTempReader : IDisposable
+{
+    private const string TempFilePath = "/sys/class/thermal/thermal_zone0/temp";
+    private readonly FileStream _tempFileStream;
+    private readonly StreamReader _tempStreamReader;
+    private bool _disposed;
+
+    public CpuTempReader()
+    {
+        _tempFileStream = new FileStream(TempFilePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        _tempStreamReader = new StreamReader(_tempFileStream);
+    }
+
+    public double? Read()
+    {
+        if (_disposed)
+            throw new ObjectDisposedException(nameof(CpuTempReader));
+        
+        _tempFileStream.Seek(0, SeekOrigin.Begin);
+        var tempStr = _tempStreamReader.ReadLine();
+
+        if (!double.TryParse(tempStr, out var tempMilliCelsius))
+            return null;
+        
+        var tempCelsius = tempMilliCelsius / 1000.0;
+        return tempCelsius;
+
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+        _disposed = true;
+        _tempStreamReader?.Dispose();
+        _tempFileStream?.Dispose();
+    }
+}
---
+++ spike_read_sensors/SpikeSensors/Program.cs
@@ -0,0 +1,18 @@
+﻿using SpikeSensors;
+using System.Reactive.Linq;
+
+
+using var cpuTempReader = new CpuTempReader();
+
+var stream = Observable.Timer(TimeSpan.Zero, TimeSpan.FromSeconds(1))
+    .Select(_ => (DateTime.Now, cpuTempReader.Read()));
+
+var subscription = stream.Subscribe(
+    x => Console.WriteLine($"[{x.Item1}] CPU Temperature: {x.Item2}°C"),
+    ex => Console.WriteLine($"Error: {ex.Message}"),
+    () => Console.WriteLine("Completed"));
+
+Console.WriteLine("Press any key to stop...");
+Console.ReadKey();
+
+subscription.Dispose();
