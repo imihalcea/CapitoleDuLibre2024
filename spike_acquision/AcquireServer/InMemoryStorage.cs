c76faabc2983180b3755ed10a733d8e136309c58
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-10-27 22:30:26 +0100

    wip acquire server

---
+++ spike_acquision/AcquireServer/InMemoryStorage.cs
@@ -0,0 +1,51 @@
+using System.Collections.Concurrent;
+namespace AcquireServer;
+
+using SerialNumber = string;
+using SensorId = string;
+
+public class InMemoryStorage
+{
+    private readonly ConcurrentDictionary<SerialNumber, Dictionary<DateTime, List<SensorData>>> _data;
+    
+    public InMemoryStorage()
+    {
+        _data = new ConcurrentDictionary<SerialNumber, Dictionary<DateTime, List<SensorData>>>();
+    }
+
+    public void Save(DeviceData deviceData)
+    {
+        if (!_data.TryGetValue(deviceData.SerialNumber, out var measures))
+        {
+            measures = new Dictionary<DateTime, List<SensorData>>();
+            _data.TryAdd(deviceData.SerialNumber, measures);
+        }
+
+        foreach (var measure in deviceData.Data)
+        {
+            if (!measures.TryGetValue(measure.At, out var values))
+            {
+                values = [];
+                measures.TryAdd(measure.At, values);
+            }
+
+            values.Add(measure.SensorData);
+        }
+    }
+    
+    public List<Measures> MeasuresByDevice(SerialNumber serialNumber)
+    {
+        if (!_data.TryGetValue(serialNumber, out var measures))
+        {
+            return new List<Measures>();
+        }
+
+        return measures.Select(m => new Measures(m.Key, m.Value)).ToList();
+    }
+    
+    public List<DeviceMeasures> AllMeasures()
+    {
+        return _data.Select(d => new DeviceMeasures(d.Key, d.Value.Select(m => new Measures(m.Key, m.Value)).ToList())).ToList();
+    }
+    
+}
---
+++ spike_acquision/AcquireServer/Model.cs
@@ -0,0 +1,15 @@
+namespace AcquireServer;
+
+using SerialNumber = string;
+using SensorId = string;
+using MeasureValue = float;
+
+public record SensorData(SensorId SensorId, MeasureValue Value);
+
+public record SensorMeasure(DateTime At, SensorData SensorData);
+
+public record DeviceData(SerialNumber SerialNumber, List<SensorMeasure> Data);
+
+public record Measures(DateTime At, List<SensorData> SensorsMeasures);
+
+public record DeviceMeasures(SerialNumber SerialNumber, List<Measures> Measures);
