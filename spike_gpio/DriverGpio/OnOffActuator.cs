5c2ab2fcc82e673b9bd538060eef3f4ea80ceb20
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-10-27 21:18:11 +0100

    gpio actuator

---
+++ spike_gpio/DriverGpio/OnOffActuator.cs
@@ -0,0 +1,38 @@
+using System.Device.Gpio;
+
+namespace DriverGpio;
+
+public class OnOffActuator: IDisposable
+{
+    private readonly int _pin;
+    private readonly GpioController _controller;
+    private bool _disposed;
+    
+    public OnOffActuator(int pin)
+    {
+        _pin = pin;
+        _controller = new GpioController();
+        _controller.OpenPin(_pin, PinMode.Output);
+        _disposed = false;
+    }
+    
+    public void On()
+    {
+        _controller.Write(_pin, PinValue.High);
+    }
+    
+    public void Off()
+    {
+        _controller.Write(_pin, PinValue.Low);
+    }
+    
+    
+    public void Dispose()
+    {
+        if (_disposed) return;
+        Off();
+        _controller.ClosePin(_pin);
+        GC.SuppressFinalize(this);
+        _disposed = true;
+    }
+}
---
+++ spike_gpio/DriverGpio/Program.cs
@@ -0,0 +1,27 @@
+﻿using DriverGpio;
+
+Console.WriteLine("Enter the pin number to control the actuator:");
+var pin = int.Parse(Console.ReadLine() ?? "6");
+
+Console.WriteLine("Commands 0=Off; 1=On; 2=Exit");
+Console.WriteLine("Enter the command:");
+
+var actuator = new OnOffActuator(pin);
+var input = Console.ReadLine();
+var order = int.Parse(input ?? "2");
+while (order != 2)
+{
+    switch (order)
+    {
+        case 0:
+            actuator.Off();
+            break;
+        case 1:
+            actuator.On();
+            break;
+    }
+
+    input = Console.ReadLine();
+    order = int.Parse(input ?? "2");
+}
+actuator.Dispose();
