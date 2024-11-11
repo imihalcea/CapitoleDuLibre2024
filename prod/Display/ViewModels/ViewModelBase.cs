2d5c304bc6899d7e6d2ab7e3cecc3ad37d28610f
Olivier Azeau <git@azeau.com>
2024-11-11 15:15:44 +0100

    Ajout Display/Desktop+Browser vierges, juste après dotnet new avalonia.xplat -n Display -o .

---
+++ prod/Display/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Display.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ prod/Display/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Display.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
