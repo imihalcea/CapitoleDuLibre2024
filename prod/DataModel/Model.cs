ef957387e290349f7aa3931447f768a035560e42
Ionut Mihalcea <ionut.mihalcea@gmail.com>
2024-11-11 14:45:11 +0100

    add sensor ids in the datamodel

--- prod/DataModel/Model.cs
+++ prod/DataModel/Model.cs
@@ -13,3 +13,16 @@ public record DeviceData(SerialNumber SerialNumber, List<SensorMeasure> Data);
 public record Measures(DateTime At, List<SensorData> SensorsMeasures);
 
 public record DeviceMeasures(SerialNumber SerialNumber, List<Measures> Measures);
+
+public class SensorIds
+{
+    public SensorId Temp => "temp";
+    public SensorId CpuLoad1 => "load_1";
+    public SensorId CpuLoad2 => "load_2";
+    public SensorId CpuLoad3 => "load_3";
+    public SensorId NetTx => "net_tx";
+    public SensorId NetRx => "net_rx";
+    
+    public List<SensorId> All => [Temp, CpuLoad1, CpuLoad2, CpuLoad3, NetTx, NetRx];
+    
+}
