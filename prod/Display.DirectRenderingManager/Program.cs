a8e8989d9af38b6a28d5ebdc89d34902877175fe
Olivier Azeau <git@azeau.com>
2024-11-13 19:28:42 +0100

    add Display.DirectRenderingManager

---
+++ prod/Display.DirectRenderingManager/Program.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia;
+using Avalonia.Media;
+
+namespace Display.DirectRenderingManager;
+
+class Program
+{
+  [STAThread]
+  public static void Main(string[] args) => BuildAvaloniaApp()
+    .StartLinuxDrm(args, card: "/dev/dri/card1", scaling: 1.0);
+  
+  public static AppBuilder BuildAvaloniaApp()
+    => AppBuilder.Configure<App>()
+      .WithInterFont()
+      .With(new FontManagerOptions
+      {
+        DefaultFamilyName = "avares://Avalonia.Fonts.Inter/Assets#Inter"
+      })
+      .LogToTrace();
+
+}
