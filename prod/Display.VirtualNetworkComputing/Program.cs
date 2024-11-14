bd28c2b19a24480a507c7ae0f5d8ce995a5820e2
Olivier Azeau <git@azeau.com>
2024-11-15 00:59:20 +0100

    add VNC display

---
+++ prod/Display.VirtualNetworkComputing/Program.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Display.VirtualNetworkComputing;
+
+class Program
+{
+  [STAThread]
+  public static void Main(string[] args) => BuildAvaloniaApp()
+    .StartWithHeadlessVncPlatform("0.0.0.0", 5900, args, ShutdownMode.OnMainWindowClose);
+  
+  public static AppBuilder BuildAvaloniaApp()
+    => AppBuilder.Configure<App>()
+      .UseSkia()
+      .WithInterFont()
+      .LogToTrace();
+
+}
